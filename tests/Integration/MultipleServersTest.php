76dfc158aaf73641eec22a786dd50edb1e23e716
diosmosis <benaka@piwik.pro>
2014-10-10 16:20:18 -0700

    Fixing MultipleServersTest.

--- tests/Integration/MultipleServersTest.php
+++ tests/Integration/MultipleServersTest.php
@@ -64,16 +64,16 @@ class MultipleServersTest extends LdapIntegrationTest
     {
         Config::getInstance()->LoginLdap['servers'] = array('dummyserver1', 'dummyserver2');
 
-        $this->doAuthTest();
+        $this->doAuthTest($expectCode = 0);
     }
 
-    private function doAuthTest()
+    private function doAuthTest($expectCode = 1)
     {
         $ldapAuth = LdapAuth::makeConfigured();
         $ldapAuth->setLogin(self::TEST_LOGIN);
         $ldapAuth->setPassword(self::TEST_PASS);
         $authResult = $ldapAuth->authenticate();
 
-        $this->assertEquals(1, $authResult->getCode());
+        $this->assertEquals($expectCode, $authResult->getCode());
     }
 }
