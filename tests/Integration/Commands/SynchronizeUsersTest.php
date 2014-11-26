abd618a2958a4b7bcfa5d007940f4092a0f95a40
diosmosis <benaka@piwik.pro>
2014-11-25 17:44:56 -0800

    Fix last remaining test.

--- tests/Integration/Commands/SynchronizeUsersTest.php
+++ tests/Integration/Commands/SynchronizeUsersTest.php
@@ -56,7 +56,7 @@ class SynchronizeUsersTest extends LdapIntegrationTest
         $this->assertEquals(0, $result, $this->getCommandDisplayOutputErrorMessage());
 
         $users = $this->getLdapUserLogins();
-        $this->assertEquals(array('blackwidow', 'captainamerica', 'ironman', 'msmarvel', 'thor'), $users);
+        $this->assertEquals(array('blackwidow', 'captainamerica', 'ironman', 'msmarvel', 'rogue@xmansion.org', 'thor'), $users);
     }
 
     public function test_CommandSynchronizesOneUser_WhenLoginSpecified()
