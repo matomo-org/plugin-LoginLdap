9de07ef8e28d59e6f8a6177a65a90426717114d6
woetzens <nils.woetzel@h-its.org>
2015-06-18 13:48:50 +0200

    UserAccessMapper: - now handles the membershipAccessStrings from the configuration - allSites also store the site-name so that they can be used for mapping besides the id (makes the rules more readable) UserSynchronizer: - synchronizePiwikAccessFromLdap translates the user name to the expected user name (would not work for users with suffix) - synchronizePiwikAccessFromLdap does not return when the access is empty, since this might be intended and should delete all access for the user - synchronizePiwikAccessFromLdap also set the superUserAccess to false before synchronizing (otherwise, the user will have super user access forever, even if revoked in the ldap attribute) index.twig: - reorganized the order of the config attributes for the membership access rights - Clarified translation that the 'Enable User Access Synchronization from LDAP' does not require to change the schema, if the ldap group memberships are used

--- LdapInterop/UserAccessMapper.php
+++ LdapInterop/UserAccessMapper.php
@@ -67,9 +67,30 @@ class UserAccessMapper
     private $userAccessAttributeParser;
 
     /**
+     * The ldap group memberships, that defines the super access status of a user
+     *
+     * @var array of groups
+     */
+    private $membershipSuperAccess = array();
+
+    /**
+     * The ldap group memberships, that define view access rights to sites
+     *
+     * @var multikey key is ldap group and value is an array of site-ids
+     */
+    private $membershipSiteViewAccess = array();
+
+    /**
+     * The ldap group memberships, that define admin access rights to sites
+     *
+     * @var multikey key is ldap group and value is an array of site-ids
+     */
+    private $membershipSiteAdminAccess = array();
+
+    /**
      * Cache for all site IDs. Set once by {@link getAllSites()}.
      *
-     * Maps int site IDs w/ unspecified data.
+     * Maps int site IDs w/ the site names.
      *
      * @var array
      */
@@ -128,10 +149,31 @@ class UserAccessMapper
             $this->addSiteAccess($sitesByAccess, 'admin', $ldapUser[$this->adminAttributeName]);
         }
 
+        // site access by group memberships
+        if( key_exists("groups", $ldapUser)) {
+            $groups = $ldapUser["groups"];
+            foreach( $groups as $group) {
+                if( key_exists($group, $this->membershipSiteViewAccess)) {
+                    foreach( $this->membershipSiteViewAccess[$group] as $siteId) {
+                        $sitesByAccess[$siteId] = 'view';
+                    }
+                }
+            }
+            foreach( $groups as $group) {
+                if( key_exists($group, $this->membershipSiteAdminAccess)) {
+                    foreach( $this->membershipSiteAdminAccess[$group] as $siteId) {
+                        $sitesByAccess[$siteId] = 'admin';
+                    }
+                }
+            }
+        }
+
+        // invert siteByAccess to accessBySite meaning siteId => accesslevel to accessLevel => array(siteIds)
         $accessBySite = array();
         foreach ($sitesByAccess as $site => $access) {
             $accessBySite[$access][] = $site;
         }
+
         return $accessBySite;
     }
 
@@ -215,6 +257,113 @@ class UserAccessMapper
         $this->userAccessAttributeParser = $userAccessAttributeParser;
     }
 
+    /**
+     * Return the {@link $membershipSuperAccess} property
+     *
+     * @return \Piwik\Plugins\LoginLdap\LdapInterop\array
+     */
+    public function getMembershipSuperAccess()
+    {
+        return $this->membershipSuperAccess;
+    }
+
+    /**
+     * Set the {@link $membershipSuperAccess} property from a ";" separated string
+     *
+     * @param string $membershipSuperAccess group names separated by ";"
+     */
+    public function setMembershipSuperAccess($membershipSuperAccess)
+    {
+        $this->membershipSuperAccess = explode(";", $membershipSuperAccess);
+    }
+
+    /**
+     * Return the {@link $membershipSiteViewAccess} property as multikey array
+     *
+     * @return \Piwik\Plugins\LoginLdap\LdapInterop\multikey
+     */
+    public function getMembershipSiteViewAccess()
+    {
+        return $this->membershipSiteViewAccess;
+    }
+
+    /**
+     * Set the {@link $membershipSiteViewAccess} property from a ";" separated string
+     *
+     * @param unknown $membershipSiteViewAccess
+     */
+    public function setMembershipSiteViewAccess($membershipSiteViewAccess)
+    {
+        $this->membershipSiteViewAccess = $this->parseMembershipSiteAccessString($membershipSiteViewAccess);
+    }
+
+    /**
+     * Return the {@link $membershipSiteAdminAccess} property as multikey array
+     *
+     * @return \Piwik\Plugins\LoginLdap\LdapInterop\multikey
+     */
+    public function getMembershipSiteAdminAccess()
+    {
+        return $this->membershipSiteAdminAccess;
+    }
+
+    /**
+     * Set the {@link $membershipSiteAdminAccess} property from a ";" separated string
+     *
+     * @param unknown $membershipSiteAdminAccess
+     */
+    public function setMembershipSiteAdminAccess($membershipSiteAdminAccess)
+    {
+        $this->membershipSiteAdminAccess = $this->parseMembershipSiteAccessString($membershipSiteAdminAccess);
+    }
+
+    /**
+     * Parses membershipSiteViewAccess or membershipSiteAdminAccess string into an easily handled multikey array with group as key and array of siteIDs as value
+     *
+     * @param string $membershipSiteAccessString a string of the form {group1}:1,test.org;{group2}:2,www.example.com defining the ldap group and associated site (by id or name)
+     * @return multitype:multitype: array
+     */
+    private function parseMembershipSiteAccessString($membershipSiteAccessString) {
+        // set of all sites with siteId and siteName
+        $setOfAllSites = $this->getSetOfAllSites();
+
+        // array with group as key and an array of siteIds as value
+        $membershipSiteAccess = array();
+
+        $groups_sites = explode( ";", $membershipSiteAccessString);
+        foreach( $groups_sites as $group_sites) {
+            list($group, $sites) = explode(":", $group_sites);
+            if( $group == NULL || $sites == NULL) {
+                continue;
+            }
+            if( !key_exists($group, $membershipSiteAccess)) {
+                $membershipSiteAccess[$group] = array();
+            }
+            $siteIds = explode(",", $sites);
+            foreach( $siteIds as $site) {
+                // if @site is a siteId
+                if( key_exists($site, $setOfAllSites)) {
+                    array_push($membershipSiteAccess[$group], $site);
+                } else { // the site could be a site name
+                    $siteId = array_search($site, $setOfAllSites);
+                    if ($siteId !== false) { // there is a site with that name
+                        array_push($membershipSiteAccess[$group], $siteId);
+                    } else {
+                        $this->logger->debug("UserAccessMapper::{func}(): site with id or name of '{site}' does not ".
+                                "exist as defined in membership site access string '{membershipSiteAccessString}', ignoring",
+                           array(
+                            'func' => __FUNCTION__,
+                            'site' => $site,
+                            'membershipSiteAccessString' => $membershipSiteAccessString
+                        ));
+                    }
+                }
+            }
+        }
+
+        return $membershipSiteAccess;
+    }
+
     private function addSiteAccess(&$sitesByAccess, $accessLevel, $viewAttributeValues)
     {
         if (!is_array($viewAttributeValues)) {
@@ -260,9 +409,15 @@ class UserAccessMapper
     private function getSetOfAllSites()
     {
         if ($this->allSites === null) {
-            $this->allSites = array_flip(Access::doAsSuperUser(function () {
-                return SitesManagerAPI::getInstance()->getSitesIdWithAtLeastViewAccess();
-            }));
+            $this->allSites = Access::doAsSuperUser(function () {
+                $siteIds = SitesManagerAPI::getInstance()->getSitesIdWithAtLeastViewAccess();
+                $sites = array();
+                foreach( $siteIds as $siteId){
+                    $site = SitesManagerAPI::getInstance()->getSiteFromId($siteId);
+                    $sites[$siteId] = $site["name"];
+                }
+                return $sites;
+            });
         }
 
         return $this->allSites;
@@ -270,6 +425,10 @@ class UserAccessMapper
 
     private function isSuperUserAccessGrantedForLdapUser($ldapUser)
     {
+        if ($this->isSuperUserAccessGrantedForLdapUserThroughMembership($ldapUser)) {
+            return true;
+        }
+
         if (!array_key_exists($this->superuserAttributeName, $ldapUser)) {
             return false;
         }
@@ -284,7 +443,26 @@ class UserAccessMapper
                 return true;
             }
         }
+
         return false;
+    }
+
+    /**
+     * ldap group memberships defined in the config param 'membership_super_access' defines a super user
+     * test if $ldapUser has a key 'groups' and if this contains a group that is also defined in the
+     * 'membership_super_access' config param
+     *
+     * @param array|multikey $ldapUser
+     * @return boolean
+     */
+    private function isSuperUserAccessGrantedForLdapUserThroughMembership($ldapUser)
+    {
+        // memberships are defined through groups key
+        if (!array_key_exists('groups', $ldapUser)) {
+            return false;
+        }
+        // there should be at least one group in the intersect
+        return ( count( array_intersect( $ldapUser['groups'], $this->membershipSuperAccess)) != 0);
     }
 
     /**
@@ -311,6 +489,21 @@ class UserAccessMapper
         $superuserAttributeName = Config::getSuperUserAccessField();
         if (!empty($superuserAttributeName)) {
             $result->setSuperuserAttributeName($superuserAttributeName);
+        }
+
+        $membershipSuperAccess = Config::getMembershipSuperAccess();
+        if (!empty($membershipSuperAccess)) {
+            $result->setMembershipSuperAccess($membershipSuperAccess);
+        }
+
+        $membershipSiteViewAccess = Config::getMembershipSiteViewAccess();
+        if (!empty($membershipSiteViewAccess)) {
+            $result->setMembershipSiteViewAccess($membershipSiteViewAccess);
+        }
+
+        $membershipSiteAdminAccess = Config::getMembershipSiteAdminAccess();
+        if (!empty($membershipSiteAdminAccess)) {
+            $result->setMembershipSiteAdminAccess($membershipSiteAdminAccess);
         }
 
         return $result;
