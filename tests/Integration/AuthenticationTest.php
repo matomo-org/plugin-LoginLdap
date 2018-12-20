5e0311e43e69d71090a433f86f50081c7bd72e68
Thomas Steur <tsteur@users.noreply.github.com>
2018-12-21 11:10:08 +1300

    hoping to fix tests

--- tests/Integration/AuthenticationTest.php
+++ tests/Integration/AuthenticationTest.php
@@ -12,6 +12,7 @@ use Piwik\AuthResult;
 use Piwik\Common;
 use Piwik\Config;
 use Piwik\Db;
+use Piwik\Piwik;
 use Piwik\Plugins\LoginLdap\Auth\LdapAuth;
 use Piwik\Plugins\UsersManager\API as UsersManagerAPI;
 use Piwik\Tests\Framework\Fixture;
@@ -178,8 +179,9 @@ class AuthenticationTest extends LdapIntegrationTest
 
     public function test_LdapAuth_AuthenticatesSuccessfully_WhenAuthenticatingNormalPiwikNonSuperUser()
     {
+
         UsersManagerAPI::getInstance()->addUser('pcoulson', 'thispasswordwillbechanged', 'pcoulson@shield.org', $alias = false);
-        UsersManagerAPI::getInstance()->updateUser('pcoulson', 'vintage', false, false, false, Fixture::getTokenAuth());
+        UsersManagerAPI::getInstance()->updateUser('pcoulson', 'vintage', false, false, false, Piwik::getCurrentUserTokenAuth());
 
         $ldapAuth = LdapAuth::makeConfigured();
         $ldapAuth->setLogin('pcoulson');
