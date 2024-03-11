5a104f83557154910b5b2a21e2b16ef7495acc72
Altamash Shaikh <altu9594@gmail.com>
2024-03-11 07:04:21 +0530

    More level down for path

--- Commands/CheckDirectDependencyUse.php
+++ Commands/CheckDirectDependencyUse.php
@@ -98,7 +98,7 @@ class CheckDirectDependencyUse extends ConsoleCommand
             $regex = '\\b' . preg_quote($prefix) . '_';
         }
 
-        $command = 'rg \'' . $regex . '\' --glob=*.php ' . $vendorScan . ' --json --sort path ' . dirname(__FILE__, 4);
+        $command = 'rg \'' . $regex . '\' --glob=*.php ' . $vendorScan . ' --json --sort path ' . dirname(__FILE__, 5);
         echo PHP_EOL . $command . PHP_EOL;
         exec($command, $rgOutput, $returnCode);
 
