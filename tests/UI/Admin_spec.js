2d7ee1df039bb36d87b0dad7beb46d48ee80b17c
Altamash Shaikh <altu9594@gmail.com>
2025-02-28 11:12:53 +0530

    Fixes for password confirmation to work

--- tests/UI/Admin_spec.js
+++ tests/UI/Admin_spec.js
@@ -69,7 +69,7 @@ describe("LoginLdap_Admin", function () {
         await page.goto(addNewTokenUrl);
         await page.waitForNetworkIdle();
         await page.waitForTimeout(30000);
-        var elem = await page.jQuery('#loginPage');
+        var elem = await page.jQuery('.page');
         expect(await elem.screenshot()).to.matchImage('addNewToken_without_password');
     });
 });
