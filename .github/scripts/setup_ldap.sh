bc05afb0e27e23871b0cbb438f2d742dafbe21d3
Altamash Shaikh <altu9594@gmail.com>
2025-03-06 10:47:33 +0530

    More changes

--- .github/scripts/setup_ldap.sh
+++ .github/scripts/setup_ldap.sh
@@ -54,7 +54,10 @@ dn: cn=module,cn=config
 objectClass: olcModuleList
 cn: module
 olcModulePath: /usr/lib/ldap
+olcModuleLoad: back_hdb
 olcModuleLoad: syncprov.la
+olcModuleLoad: memberof.la
+olcModuleLoad: refint.la
 
 
 # database
